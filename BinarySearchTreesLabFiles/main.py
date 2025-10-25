c2e70ba29b81813b480c098df5b48f177e6ae6cc
Conner Diers <codier01@wsc.edu>
2025-10-24 20:11:20 -0500

    Delete BinarySearchTreesLabFiles/main.py

--- BinarySearchTreesLabFiles/main.py
+++
@@ -1,6 +0,0 @@
-# main.py
-from bst import BST
-tree = BST([10, 5, 15, 2, 7, 18])
-print("Search 7:", tree.search(7)) # True
-print("Search 99:", tree.search(99)) # False
-print("Comparisons so far:", tree.comparisons)
